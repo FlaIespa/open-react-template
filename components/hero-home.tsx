ed500336c3ee012942b87b9b9b3d2617f12548f9
Flavia Iespa <flavia.iespa@uni.minerva.edu>
2024-10-07 14:00:01 -0700

    adds the slack links and fixes boldness.

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -27,7 +27,10 @@ export default function HeroHome() {
                 <div data-aos="fade-up" data-aos-delay={400}>
                   <a
                     className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                    href="#0"
+                    href="https://join.slack.com/t/id8crew/shared_invite/zt-2s1mgsy05-bLf1AhWYEDU82W2Tm14q7g"
+                    target="_blank"
+                    rel="noopener noreferrer" 
+
                   >
                     <span className="relative inline-flex items-center">
                       Join the Community
